7ca46f8124ccf964fa02b999952aa870ddadb0d4
aidonmccomb <101207078+aidonmccomb@users.noreply.github.com>
2022-04-19 14:20:15 -0400

    first commit

---
+++ Calcubot2/Calcubot2/Calcubot2App.swift
@@ -0,0 +1,17 @@
+//
+//  Calcubot2App.swift
+//  Calcubot2
+//
+//  Created by Student on 2022-04-19.
+//
+
+import SwiftUI
+
+@main
+struct Calcubot2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calcubot2/Calcubot2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calcubot2
+//
+//  Created by Student on 2022-04-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
