1e66df63f9b220d184a54a84d7e7bbacde789a2f
aidonmccomb <101207078+aidonmccomb@users.noreply.github.com>
2022-04-20 09:09:21 -0400

    great work!

---
+++ Calcubot2/Calcubot2/GearRatio.swift
@@ -0,0 +1,20 @@
+//
+//  GearRatio.swift
+//  Calcubot2
+//
+//  Created by Student on 2022-04-19.
+//
+
+import SwiftUI
+
+struct GearRatio: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GearRatio_Previews: PreviewProvider {
+    static var previews: some View {
+        GearRatio()
+    }
+}
