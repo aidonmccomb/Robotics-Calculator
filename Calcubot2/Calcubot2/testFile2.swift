710bf4ee6d06d36df3ed06d068fc81baa4f52b75
aidonmccomb <101207078+aidonmccomb@users.noreply.github.com>
2022-04-22 11:08:00 -0400

    picker not working :(

---
+++ Calcubot2/Calcubot2/testFile2.swift
@@ -0,0 +1,28 @@
+//
+//  testFile2.swift
+//  Calcubot2
+//
+//  Created by Student on 2022-04-22.
+//
+
+import SwiftUI
+
+struct testFile2: View {
+    @State var selUnit = MetDistUnits.m
+    
+    var body: some View {
+        VStack {
+            Picker("Units", selection: $selUnit, content: {
+                ForEach(MetDistUnits.allCases, content: {
+                    descript in Text(description.captailized)
+                })
+            })
+        }
+    }
+}
+
+struct testFile2_Previews: PreviewProvider {
+    static var previews: some View {
+        testFile2()
+    }
+}
