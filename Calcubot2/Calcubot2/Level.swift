887c394f82de828744d849bcdb1f204f6c2493fc
aidonmccomb <101207078+aidonmccomb@users.noreply.github.com>
2022-04-28 13:35:53 -0400

    commit level

--- Calcubot2/Calcubot2/Level.swift
+++ Calcubot2/Calcubot2/Level.swift
@@ -7,44 +7,44 @@
 
 import SwiftUI
 
-struct Level: View {
-    @State var speed: Double = 180.0
-    @State private var isEditing = false
-    
-    var overlay: some View {
-        VStack{
-            Text("Level")
-                .bold()
-                .foregroundColor(Color.lightGrey)
-            ZStack{
-                Circle()
-                    .foregroundColor(Color.white)
-                    .frame(width: 300, height: 300, alignment: .center)
-                RoundedRectangle(cornerRadius: 5)
-                    .foregroundColor(Color.red)
-                    .rotationEffect(Angle(degrees: Double(speed)))
-                    .frame(width: 280, height: 10, alignment: .center)
-            }
-            Slider(
-                value: $speed,
-                in: 0...360,
-                onEditingChanged: { editing in
-                    isEditing = editing
-                }
-            )
-        }
-    }
-    var body: some View {
-        NavigationView {
-            Rectangle().fill(Color.myBackGround)
-                .edgesIgnoringSafeArea(.all)
-                .overlay(overlay)
-        }
-    }
-}
+ struct Level: View {
+     @State var speed: Double = 180.0
+     @State private var isEditing = false
 
-struct Level_Previews: PreviewProvider {
-    static var previews: some View {
-        Level()
-    }
-}
+     var overlay: some View {
+         VStack{
+             Text("Level")
+                 .bold()
+                 .foregroundColor(Color.lightGrey)
+             ZStack{
+                 Circle()
+                     .foregroundColor(Color.white)
+                     .frame(width: 300, height: 300, alignment: .center)
+                 RoundedRectangle(cornerRadius: 5)
+                     .foregroundColor(Color.red)
+                     .rotationEffect(Angle(degrees: Double(speed)))
+                     .frame(width: 280, height: 10, alignment: .center)
+             }
+             Slider(
+                 value: $speed,
+                 in: 0...360,
+                 onEditingChanged: { editing in
+                     isEditing = editing
+                 }
+             )
+         }
+     }
+     var body: some View {
+         NavigationView {
+             Rectangle().fill(Color.myBackGround)
+                 .edgesIgnoringSafeArea(.all)
+                 .overlay(overlay)
+         }
+     }
+ }
+
+ struct Level_Previews: PreviewProvider {
+     static var previews: some View {
+         Level()
+     }
+ }
