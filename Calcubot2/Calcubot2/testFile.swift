5f4101c4c6f9cb832cfcc5295afd8819dc56cada
aidonmccomb <101207078+aidonmccomb@users.noreply.github.com>
2022-05-02 11:40:57 -0400

    playing with classes

--- Calcubot2/Calcubot2/testFile.swift
+++ Calcubot2/Calcubot2/testFile.swift
@@ -7,6 +7,32 @@
 
 import SwiftUI
 
+class Vehicle {
+    var isRoadLegal : Bool
+    var wheelCount : Int
+    
+    init(isRoadLegal: Bool, wheelCount: Int){
+        self.isRoadLegal = isRoadLegal
+        self.wheelCount = wheelCount
+    }
+}
+
+//super???
+
+//class Car: Vehicle{
+//    var isSportsCar: Bool
+//    var color: Color
+//
+//    init(isRoadLegal: Bool, wheelCount: Int, isSportsCar: Bool, color: Color) {
+//        super.init(isRoadLegal: Bool, wheelCount: Int) {
+//        super.self.isRoadLegal = true
+//        super.self.wheelCount = 4
+//        }
+//        self.isSportsCar = isSportsCar
+//        self.color = color
+//    }
+//}
+
 struct testFile: View {
     var body: some View {
         Text("test file")
